71807d5bc4958e8c9f54ea8e2397cad0876b76c2
ATirado <azuru5039@gmail.com>
2024-06-05 15:41:55 -0400

    Update App component to include NavBar, Hero, HomeCards, and JobListings

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <NavBar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
 
-export default App
+export default App;
